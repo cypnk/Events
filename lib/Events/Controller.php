5d0dac2236e99e6d5c541b33ffa36daf7cf30701
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2024-01-19 00:07:47 -0500

    Update Controller.php

--- lib/Events/Controller.php
+++ lib/Events/Controller.php
@@ -5,26 +5,80 @@ namespace Events;
 class Controller {
 	
 	/**
-	 *  Loaded events
-	 *  @var array
+	 *  Application error file
+	 *  @var string
 	 */
-	protected array $events	= [];
+	protected readonly string $errfile;
+	
+	/**
+	 *  Visitor error logging file
+	 *  @var string
+	 */
+	protected readonly string $verrfile;
+	
+	/**
+	 *  Logging notice file
+	 *  @var string
+	 */
+	protected readonly string $notefile;
+	
+	/**
+	 *  Storage directory path
+	 *  @var string
+	 */
+	protected readonly string $writable;
+	
+	/**
+	 *  Maximum log file size before rolling over
+	 */
+	const	MAX_LOG_SIZE		= 5000000;
 	
 	/**
 	 *  Shared parameters
 	 *  @var array
 	 */
-	protected array $params;
+	protected array $params		= [];
 	
 	/**
-	 *  Initialize controller
-	 *  
-	 *  @param array	$_params	Startup classes by fully qualified name
+	 *  Loaded events
+	 *  @var array
 	 */
-	public function __construct( ?array $_params = null ) {
-		if ( empty( $_params ) ) { return; }
+	protected array $events		= [];
+	
+	/**
+	 *  Log data storage
+	 *  @var array
+	 */
+	protected array $logs		= [];
+	
+	public function __construct( 
+		string	$_write, 
+		?string	$_errfile	= null,
+		?string	$_verrfile	= null,
+		?string $_notefile	= null,
+	) {
+		$this->writable	= \rtrim( $_write, '/\\' ) . '/';
 		
-		$this->addParams( $_params );
+		$this->errfile	= $_errfile	?? 'errors.log';
+		$this->verrfile	= $_verrfile	?? 'visitor_errors.log';
+		$this->notefile	= $_notefile	?? 'notices.log';
+		
+		\set_exception_handler( [ $this, 'exceptionHandler' ] );
+		\set_error_handler( [ $this, 'errorHandler' ] );
+	}
+	
+	public function __destruct() {
+		\restore_exception_handler();
+		\restore_error_handler();
+		
+		$this->logMessages();
+	}
+	
+	public function storage( ?string $_path = null ) {
+		return 
+		empty( $_path ) ? 
+			$this->writable : 
+			$this->writable . \ltrim( $_path, '/\\' );
 	}
 	
 	/**
@@ -32,23 +86,7 @@ class Controller {
 	 *  
 	 *  @param array	$_params	Loading parameters
 	 */
-	public function addParams( array $_params ) {
-		
-		set_error_handler( function( 
-			$eno, $emsg, $efile, $eline 
-		) {
-			$str	= 
-			'Error adding controller parameters ' .  
-				'Message: {msg} File: {file} Line: {line}';
-			
-			logException( 
-				new \ErrorException( 
-					$emsg, 0, $eno, $efile, $eline 
-				), $str 
-			);
-		}, E_WARNING | E_ERROR );
-		
-		
+	public function addParam( array $_params ) {
 		foreach ( $_params as $p ) {
 			// Only handle strings and objects
 			if ( !\is_string( $p ) || !\is_object( $p ) ) {
@@ -56,13 +94,14 @@ class Controller {
 			}
 			
 			// Filter type
-			$t = match( \strtolower( \gettype( $p ) ) ) {
+			$t = 
+			match( \strtolower( \gettype( $p ) ) ) {
 				'string'	=> 'string', 
 				'object', 'resource', 'resource (closed)' 
 						=> 'object', 
 				// Skip all else
 				default		=> 'skip'
-			}
+			};
 			
 			// Shouldn't be handled
 			if ( 0 == \strcmp( 'skip', $t ) ) {
@@ -98,8 +137,6 @@ class Controller {
 				default			=> $p
 			}
 		}
-		
-		\restore_error_handler();
 	}
 	
 	/**
@@ -113,13 +150,15 @@ class Controller {
 	}
 	
 	/**
-	 *  Add to list of running events
+	 *  Add runnable event to current list
 	 *  
-	 *  @param string	$name		New event name
-	 *  @param Handler	$handler	
+	 *  @param string	$name	Unique event name
 	 */
 	public function listen( string $name, Handler $handler ) {
-		$this->addEvent( $name );
+		if ( !\array_key_exists( $name, $this->events ) ) {
+			$this->events[$name]	= new Event( $this, $name );
+		}
+		
 		$this->events[$name]->attach( $handler );
 	}
 	
@@ -130,12 +169,13 @@ class Controller {
 	 *  @param object	$handler	Event handler
 	 *  @reutrn bool
 	 */
-	public function dismiss( string $name, Handler $handler ) : bool {
-		if ( \array_key_exists( $name, $this->events ) ) {
-			$this->events[$name]->detach( $handler );
-			return true;
+	public function dismiss( string $name, Handler $handler ) {
+		if ( !\array_key_exists( $name, $this->events ) ) {
+			return false;
 		}
-		return false;
+		
+		$this->events[$name]->detach( $handler );
+		return true;
 	}
 	
 	/**
@@ -144,25 +184,13 @@ class Controller {
 	 *  @param string	$name	Event name
 	 *  @return bool
 	 */
-	public function removeEvent( string $name ) : bool {
-		if ( \array_key_exists( $name, $this->events ) ) {
-			unset( $this->events[$name] );
-			return true;
-		}
-		return false;
-	}
-	
-	/**
-	 *  Add runnable event to current list
-	 *  
-	 *  @param string	$name	Unique event name
-	 */
-	public function addEvent( string $name ) {
-		if ( \array_key_exists( $name, $this->events ) ) {
-			return;
+	public function unregister( string $name ) : bool {
+		if ( !\array_key_exists( $name, $this->events ) ) {
+			return false;
 		}
 		
-		$this->events[$name] = new Event( $this, $name );
+		unset( $this->events[$name] );
+		return true;
 	}
 	
 	/**
@@ -172,9 +200,218 @@ class Controller {
 	 *  @param array	$params		Runtime parameters
 	 */
 	public function run( string $name, ?array $params = null ) {
-		if ( \array_key_exists( $name, $this->events ) ) {
-			$this->events[$name]->notify( $params );
+		if ( !\array_key_exists( $name, $this->events ) ) {
+			return;
+		}
+		
+		$this->events[$name]->notify( $params );
+	}
+	
+	/**
+	 *  User input and environment data filtering helper
+	 *  
+	 *  @param string	$source		Data source type, defaults to 'get'
+	 *  @param array	$filter		Input processing filters
+	 */
+	public function inputData( string $source, array $filter ) : array {
+		$dtype	= 
+		match( \strtolower( $source ) ) {
+			'post'			=> \INPUT_POST,
+			'cookie'		=> \INPUT_COOKIE,
+			'server'		=> \INPUT_SERVER,
+			'env', 'environment'	=> \INPUT_ENV,
+			default			=> \INPUT_GET
+		};
+		
+		$data	= \filter_input_array( $dtype, $filter, true );
+		return empty( $data ) ? [] : $data;
+	}
+	
+	/**
+	 *  Error recording helper
+	 *  
+	 *  @param int		$ecode		Error code
+	 *  @param string	$estr		Error message
+	 *  @param string	$efile		Origininating file name
+	 *  @param int		$eline		Error file line location
+	 */
+	public function errorHandler( 
+		int	$ecode, 
+		string	$estr, 
+		string	$efile	= null, 
+		int	$eline 
+	) {
+		$this->error( 
+			new \ErrorException( $estr, -1, $ecode, $efile, $line ),
+			true
+		);
+	}
+	
+	/**
+	 *  Exception recording helper
+	 *  
+	 *  @param Exception	$e	Thrown error
+	 */
+	public function exceptionHandler( \Throwable $e ) {
+		$this->error( $e, true );
+	}
+	
+	/**
+	 *  Capture error for logging
+	 *  
+	 *  @param mixed	$err	Error message or exception to store
+	 *  @param bool		$app	Application error if true, visitor error if false	
+	 */
+	public function error( $err, bool $app = true ) : void {
+		$msg	= 
+		match( true ) {
+			
+			// Thrown exception
+			( $err instanceof \Exception )	=> 
+			\strtr( 'Exception: {msg} in {file} on line {line}', [
+				'{msg}'		=> $err->getMessage(),
+				'{file}'	=> $err->getFile(),
+				'{line}'	=> $err->getLine()
+			] ), 
+			
+			// Generic capture from E.G. error_get_last()
+			\is_array( $err )		=> 
+			\strtr( '{type}: {msg} in {file} on line {line}', [
+				'{type}'	=> $err['type']		?? 'Unkown type',
+				'{msg}'		=> $err['message']	?? 'No message',
+				'{file}'	=> $err['file']		?? 'Unknown file',
+				'{line}'	=> $err['line']		?? 'Unkown line'
+			] ), 
+			
+			default				=> ( string ) $err	
+		};
+		
+		if ( $app ) {
+			$this->message( 'error', $msg );
+			return;
+		}
+		
+		$filter	= [
+			'SERVER_ADDR'		=> [
+				'filter'	=> \FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+				'flags'		=> \FILTER_REQUIRE_SCALAR
+			],
+			'REQUEST_METHOD'	=> [
+				'filter'	=> \FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+				'flags'		=> \FILTER_REQUIRE_SCALAR
+			],
+			
+			'REQUEST_URI'		=> [
+				'filter'	=> \FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+				'flags'		=> \FILTER_REQUIRE_SCALAR
+			],
+			
+			'HTTP_USER_AGENT'	=> [
+				'filter'	=> \FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+				'flags'		=> \FILTER_REQUIRE_SCALAR
+			]
+		];
+		
+		$data	= $this->inputData( 'server', $filter );
+		$msg	.= ' ' . 
+			$data['SERVER_ADDR']		?? 'unknown-host ' . 
+			$data['REQUEST_METHOD']		?? 'unknown-method ' . 
+			$data['REQUEST_URI']		?? 'unknown-uri ' . 
+			$data['HTTP_USER_AGENT']	?? 'unknown-user-agent';
+		
+		$this->message( 'visitor', $msg );
+	}
+	
+	/**
+	 *  Check log file size and rollover, if needed
+	 *  
+	 *  @param string	$file	Log file name
+	 */
+	protected function logRollover( $file ) {
+		// Nothing to rollover
+		if ( !\file_exists( $file ) ) {
+			return;
+		}
+		
+		$fs	= \filesize( $file );
+		
+		// Empty file
+		if ( false === $fs ) {
+			return;
+		}
+		
+		if ( $fs > static::MAX_LOG_SIZE ) {
+			$f = \rtrim( \dirname( $file ), '/\\' ) . 
+				\DIRECTORY_SEPARATOR . 
+				\basename( $file ) . '.' . 
+				\gmdate( 'Ymd\THis' ) . '.log';
+			\rename( $file, $f );
+		}
+	}
+	
+	function logToFile( string $msg, string $dest ) {
+		$this->logRollover( $dest );
+		\error_log( 
+			\gmdate( 'D, d M Y H:i:s T', time() ) . "\n" . 
+				$msg . "\n\n\n\n", 
+			3, 
+			$this->storage( $dest )
+		);
+	}
+	
+	/**
+	 *  Message holder
+	 *  
+	 *  @param string	$type		Message type, determines storage location
+	 *  @param string	$message	Log content body
+	 */
+	public function message( string $type, string $message ) {
+		if ( $ret && $message ) {
+			return $this->logs;
+		}
+		
+		if ( !isset( $this->logs[$type] ) ) {
+			$this->logs[$type] = [];	
+		}
+		
+		// Clean message to file safe format
+		$this->logs[$type][] = 
+		\preg_replace( 
+			'/[\x00-\x08\x0B\x0C\x0E-\x1F\x80-\x9F' . 
+				'[\x{fdd0}-\x{fdef}\p{Cs}\p{Cf}\p{Cn}]/u', 
+			'', 
+			$message 
+		);
+	}
+	
+	public function logMessages() : void {
+		if ( empty ( $this->logs ) ) {
+			return;
+		}
+		
+		foreach ( $this->logs as $k => $v ) {
+			switch ( $k ) {
+				case 'error':
+				case 'errors':
+					foreach( $v as $m ) {
+						logToFile( $m, $this->errfile );
+					}
+					break;
+				
+				case 'visistor':
+				case 'visitors':
+					foreach( $v as $m ) {
+						logToFile( $m, $this->notefile );
+					}
+					break;
+					
+				case 'notice':
+				case 'notices':
+					foreach( $v as $m ) {
+						logToFile( $m, $this->notefile );
+					}
+					break;
+			}
 		}
 	}
 }
-
